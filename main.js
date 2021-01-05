6fb8971b84b46ae1c1887dfa1dce4086f6dda0ca
Devansh Garg <dg.devansh.dg@gmail.com>
2021-01-05 17:10:08 +0530

    mistake corrected

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const prompt = require('prompt-sync')({sigint: true});
 
 const hat = '^';
 const hole = 'O';
-const fieldCharacter = '░ ';
+const fieldCharacter = '░';
 const pathCharacter = '*';
 
 class Field {
