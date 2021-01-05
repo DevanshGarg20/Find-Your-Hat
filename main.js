14f044a7851e2b4922437ab22672051d703146c8
Devansh Garg <dg.devansh.dg@gmail.com>
2021-01-05 16:49:45 +0530

    completed the basic functioning will add dynamic nature

--- main.js
+++ main.js
@@ -8,30 +8,81 @@ const pathCharacter = '*';
 class Field {
   constructor(field){
     this.field = field;
+    this.locationX = 0;
+    this.locationY = 0;
+    this.field[0][0] = pathCharacter;
   }
+
   print(){
     for(let i = 0; i < this.field.length; i++){
-      console.log(this.field[i].join())
+      console.log(this.field[i].join(''))
     }
+  }
+
+  static generateField(height, width , percentage){
+
   }
   // helper methods
   run(){
+    let playing = true;
+    while(playing){
     this.print();
-    let currMove = this.ask();
-  }
+    this.ask();
+    if(this.isOutOfBOund()){
+      console.log('Game Over, you went out of bound');
+      playing = false;
+      break;
+    }else if(this.isHole()){
+      console.log('Game Over, you fell in the hole');
+      playing = false;
+      break;
+    }else if(this.isWinner()){
+      console.log('Hurrah! you won the game.');
+      playing = false;
+      break;
+    }
 
-  win(){
-
+    this.field[this.locationY][this.locationX] = pathCharacter;
+    }
   }
 
   ask(){
-    console.log('press:- u for up, d for down, r for right, l for left');
-    const direction = prompt('where would you like to move:- ')
-    return direction;
+    console.log('press:- U for up, D for down, R for right, L for left');
+    const direction = prompt('where would you like to move:- ').toUpperCase();
+    switch (direction) {
+      case 'U':
+        this.locationY -= 1;
+        break;
+      case 'D':
+        this.locationY += 1;
+        break;
+      case 'L':
+        this.locationX -= 1;
+        break;
+      case 'R':
+        this.locationX += 1;
+        break;
+      default:
+        console.log('Enter U, D, L or R.');
+        this.ask();
+        break;
+    }
+
   }
 
-}
+  isHole(){
+    return this.field[this.locationY][this.locationX] === hole;
+  }
+  
+  isWinner(){
+    return this.field[this.locationY][this.locationX] === hat;
+  }
 
+  isOutOfBOund(){
+    return (this.locationX < 0 || this.locationY < 0 || this.locationX >= this.field[0].length || this.locationY >= this.field.length);
+ }
+
+}
 
 const myField = new Field([
   ['*', '░', 'O'],
